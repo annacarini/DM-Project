58b15b1a9e67a146e3f086f3b88b57b8b8614db8
annacarini <carini.1771784@studenti.uniroma1.it>
2024-04-26 15:50:11 +0200

    animazione merge

--- application.js
+++ application.js
@@ -273,7 +273,7 @@ function play() {
                         applicationState = States.GroupInBuffer;
                         callback();
                     });
-                })
+                });
             }
             break;
 
@@ -343,16 +343,58 @@ function play() {
         
                 var framesToWrite = [];
 
+                // Ottieni le posizioni nel buffer dei vari frame (servono per l'animazione)
+                start_x = [];
+                start_y = [];
+                end_x = [];
+                end_y = [];
+                start_size = [];
+                end_size = [];
+                color = [];
+                var animationLength = 1000;
+
                 // Carica una pagina di ogni child dentro framesToWrite
                 for (let i = 0; i < siblings.length; i++) {
-                    framesToWrite.push(relation.readOnePageOfChild(i));
+                    var fr = relation.readOnePageOfChild(i);
+                    framesToWrite.push(fr);
+                    start_x.push(fr.x);
+                    start_y.push(fr.y);
+                    let endPos = buffer.getPositionOfFrame(i);
+                    end_x.push(endPos[0]);
+                    end_y.push(endPos[1]);
+                    start_size.push(fr.size);
+                    end_size.push(frameSize);
+                    color.push(fr.color);
                 }
-                console.log("I frame letti: ", framesToWrite)
-                // Carica nel buffer le pagine da leggere
-                buffer.read(framesToWrite)
+
+                // Crea animazione
+                animateMultipleSquares(start_x, start_y, end_x, end_y, start_size, end_size, color, animationLength, () => {
+                    // Shifta i frame in modo da riportare gli spazi vuoti all'inizio
+                    for (let i = 0; i < framesToWrite.length; i++) {
+                        relation.shiftFramesByOne(framesToWrite[i]);
+                    }
+                    // Scrivi i dati nel buffer
+                    buffer.read(framesToWrite, () => {
+                        applicationState = States.ChildrenInBuffer;
+                        callback();
+                    });
+                });
             }
 
             break;
+
+        case States.ChildrenInBuffer:
+            // di' al buffer di fare il sort. passagli due callback: una da chiamare quando
+            // uno dei frame del buffer si svuota, e ti fa andare allo stato OneEmptyFrameInBuffer,
+            // una da chiamare quando l'output frame e' pieno e ti fa andare allo stato
+            // OutputFrameFullMerging
+
+            break;
+
+        case States.OneEmptyFrameInBuffer:
+
+            break;
+
 
 
         default:
@@ -362,7 +404,7 @@ function play() {
     console.log("I'm in state: " + applicationState);
 }
 
-var automaticPlay = true;
+var automaticPlay = false;
 
 async function callback() {
     if (! automaticPlay) {
@@ -487,160 +529,3 @@ function writeSomething() {
     console.log(relation.writeWithAnimation(frameToWrite));
 }
 
-
-
-
-
-
-function init2() {
-
-    var elem = document.getElementById("simulation");
-    var centerX = window.innerWidth / 2;
-    var centerY = window.innerHeight / 2;
-
-
-    var elementNames = [
-        "",
-        "Hydrogen",
-        "Helium",
-        "Lithium",
-        "Beryllium",
-        "Boron",
-        "Carbon",
-        "Nitrogen",
-        "Oxygen",
-        "Fluorine",
-        "Neon"
-    ];
-
-    var styles = {
-        alignment: "center",
-        size: 36,
-        family: "Lato"
-    };
-
-    var nucleusCount = 10;
-    var nucleusArray = Array();
-
-    var electronCount = 10;
-    var electronArray = Array();
-
-    function intRange(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    var two = new Two({ fullscreen: true }).appendTo(elem);
-
-    var protonColor = two.makeRadialGradient(
-        0,
-        0,
-        15,
-        new Two.Stop(0, "red", 1),
-        new Two.Stop(1, "black", 1)
-    );
-
-    var neutronColor = two.makeRadialGradient(
-        0,
-        0,
-        15,
-        new Two.Stop(0, "blue", 1),
-        new Two.Stop(1, "black", 1)
-    );
-
-    for (i = 0; i < nucleusCount; i++) {
-        nucleusArray.push(two.makeCircle(intRange(-10, 10), intRange(-10, 10), 8));
-    }
-
-    nucleusArray.forEach(function(nucleus, index) {
-        if (index % 2 == 0) {
-            nucleus.fill = protonColor;
-        }
-        if (index % 2 == 1) {
-            nucleus.fill = neutronColor;
-        }
-        nucleus.noStroke();
-    });
-
-    for (var i = 0; i < 10; i++) {
-        if (i < 2) {
-            var shellRadius = 50;
-            var angle = i * Math.PI;
-            electronArray.push(
-                two.makeCircle(
-                    Math.cos(angle) * shellRadius,
-                    Math.sin(angle) * shellRadius,
-                    5
-                )
-            );
-        }
-        if (i >= 2 && i < 10) {
-            var shellRadius = 80;
-            var angle = (i - 2) * Math.PI / 4;
-            electronArray.push(
-                two.makeCircle(
-                    Math.cos(angle) * shellRadius,
-                    Math.sin(angle) * shellRadius,
-                    5
-                )
-            );
-        }
-    }
-
-    var orbitA = two.makeCircle(centerX, centerY, 50);
-    orbitA.fill = "transparent";
-    orbitA.linewidth = 2;
-    orbitA.stroke = "rgba(0, 0, 0, 0.1)";
-
-    var orbitB = two.makeCircle(centerX, centerY, 80);
-    orbitB.fill = "transparent";
-    orbitB.linewidth = 2;
-    orbitB.stroke = "rgba(0, 0, 0, 0.1)";
-
-    var groupElectronA = two.makeGroup(electronArray.slice(0, 2));
-    groupElectronA.translation.set(centerX, centerY);
-    groupElectronA.fill = "orange";
-    groupElectronA.linewidth = 1;
-
-    var groupElectronB = two.makeGroup(electronArray.slice(2, 10));
-    groupElectronB.translation.set(centerX, centerY);
-    groupElectronB.fill = "yellow";
-    groupElectronB.linewidth = 1;
-
-    var groupNucleus = two.makeGroup(nucleusArray);
-    groupNucleus.translation.set(centerX, centerY);
-
-    two
-    .bind("update", function(frameCount) {
-        groupElectronA.rotation += 0.025 * Math.PI;
-        groupElectronB.rotation += 0.005 * Math.PI;
-        groupNucleus.rotation -= 0.05;
-    })
-    .play();
-
-    var text = two.makeText("", centerX, 100, styles);
-
-    nucleusArray.forEach(function(nucleus, index) {
-        nucleus.opacity = 0;
-    });
-
-    electronArray.forEach(function(electron, index) {
-        electron.opacity = 0;
-    });
-
-    visible = 0;
-
-    document.addEventListener("click", function(event) {
-        if (visible < nucleusArray.length) {
-            nucleusArray[visible].opacity = 1;
-            electronArray[visible].opacity = 1;
-            visible++;
-            text.value = elementNames[visible];
-        }
-        else {
-            nucleusArray.forEach(el => el.opacity=0);
-            electronArray.forEach(el => el.opacity=0);
-            visible = 0;
-            text.value = elementNames[0];
-        }
-    });         
-}
